d988cc3174df2ef8df046b16a054018391398af4
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-02-05 21:13:33 +0530

    Update and rename V23forloop.java to Forloop.java

--- Forloop.java
+++ Forloop.java
@@ -1,27 +1,11 @@
 // WAP to find the odd numbers between two certain numbers.
 package com.company;
 import java.util.Scanner;
-public class V23forloop
+public class Forloop
 {
     public static void main(String[] args)
     {
-        //for (int i = 1; i<= 10; i++){
-        //  System.out.println(i);
-        //}
-       /*
-        for (int i = 1; i <= 10; i++)
-        {
-            System.out.println("*");
-        }
-
-        */
-        //int i = 10;
-        //Scanner scr = new Scanner (System.in);
-        //System.out.println("input first value");
-        //int i = scr.nextInt();
-        //System.out.println("input second value");
-        //int j = scr.nextInt();
-        // taking user input
+        
         Scanner scr = new Scanner(System.in);
         System.out.println("input first number");
         int i = scr.nextInt();
