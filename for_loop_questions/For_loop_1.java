e319a46775056558c7a303a5a19e53cb9263508f
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-02-06 22:39:35 +0530

    Update and rename question_1 to For_loop_1.java

---
+++ for_loop_questions/For_loop_1.java
@@ -0,0 +1,11 @@
+// WAP to out put number form 1 to 10.
+public class Forloop_1
+{
+  public static void main(String []args)
+  {
+    for (int i = 1; i < 11; i++)
+    {
+      System.out.println(i);
+    }
+  }
+}
