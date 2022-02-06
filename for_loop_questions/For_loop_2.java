024c522bd5214a9adbfcd317621d66bad1a41143
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-02-06 22:43:50 +0530

    Create For_loop_2.java

---
+++ for_loop_questions/For_loop_2.java
@@ -0,0 +1,11 @@
+// WAP to print number from 10 to 1
+public class For_loop_2
+{
+  public static void main (String[]args)
+  {
+    for (int i = 10; i >0; i--)
+    {
+      System.out.println(i);
+    }
+  }
+}
