039a9a1ac8554f9882fb0b5bba69fbc308e62af1
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-02-08 22:56:31 +0530

    Create For_loop_4.java

---
+++ for_loop_questions/For_loop_4.java
@@ -0,0 +1,17 @@
+public class Loops
+{
+    public static void main(String[]args)
+    {
+        for(int i=11; i<20; i++)
+        {
+            System.out.println(i);
+            if (i == 18)
+            {
+                System.out.println("hello because you are at 18");
+                break;
+            }
+            
+        }
+        System.out.println("the  loop ends here");
+    }
+}
