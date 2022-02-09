c178e2abcdb86e20cf08cfeeae0213d032c38f2e
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-02-09 23:32:31 +0530

    Create Pattern.java

---
+++ Pattern/Pattern.java
@@ -0,0 +1,21 @@
+//package com.company;
+import java.util.Scanner;
+public class pattern
+{
+    // Question_1
+    public static void main (String[]args)
+    {
+        Scanner scr = new Scanner (System.in);
+        System.out.println("How many times u want to get printed the pattern");
+        int n = scr.nextInt();
+        System.out.println("Lets create magic with stars ");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j< i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print(" \n ");
+        }
+    }
+}
